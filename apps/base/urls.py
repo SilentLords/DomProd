58c72804fb25ef1427e7be7ba12721df0f79fd31
nikitatonkoskurov <Vbhevbh22>
2020-08-27 21:18:54 +0500

    small changes

--- apps/base/urls.py
+++ apps/base/urls.py
@@ -1,5 +1,5 @@
 from .views import Reparse, Gkh, House, GetListOfStreets, IgnoreList, FavList, WatchList, GetIgnore, GetFav, GetWatch, \
-    HouseListView, CreateClientSet, GetClientSet, CreatePhotoArhive
+    HouseListView, CreateClientSet, GetClientSet, CreatePhotoArhive, HouseClient
 from django.urls import path, include
 from domofound2.yasg import urlpatterns as doc_urls
 
@@ -14,6 +14,7 @@ urlpatterns = [
     path('fav/', FavList.as_view()),
     path('watch/', WatchList.as_view()),
     path('get_house/<int:pk>', House.as_view()),
+    path('get_house_client/<int:pk>', HouseClient.as_view()),
     path('get_jkh/<int:pk>', Gkh.as_view()),
     path('create_user_set/', CreateClientSet.as_view()),
     path('get_user_set/', GetClientSet.as_view()),
